e5d0dc6504aeb3c622c6ac0c9ea860cf22c7ab53
Andres Chavez <107895172+ChavitoCdm@users.noreply.github.com>
2023-07-26 23:45:13 -0300

    Add files via upload

---
+++ bd.py
@@ -0,0 +1,12 @@
+import mysql.connector
+from mysql.connector import errors
+import datos
+
+def conectar():
+    try:
+        conn = mysql.connector.connect(**datos.credenciales)
+    except errors.DatabaseError as err:
+        print("Error al conectar a la base de datos.", err)
+    else:
+        return conn
+    
