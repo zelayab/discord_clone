5460cca3fd01d4a2c531fb17c088041d9817434b
Andres Chavez <andreschavezcdm@gmail.com>
2023-08-02 19:08:26 -0300

    todas las funciones hacen sus llamados

--- llamados.py
+++ llamados.py
@@ -2,6 +2,8 @@ import mysql.connector
 from mysql.connector import errors
 import datos
 import bd
+
+
 # funcionando
 def create_user (username,password,email):
     query = "INSERT INTO users (username,password,email) VALUES (%s,%s,%s)"
@@ -23,7 +25,7 @@ def create_server(name,description):
     conn.commit()
     conn.close()
     print(cur.rowcount, "Servidor creado")
-
+# funcionando
 def create_channel(name,server_id):
     query = "INSERT INTO channels (name,server_id) VALUES (%s,%s)"
     conn = bd.conectar()
@@ -33,7 +35,7 @@ def create_channel(name,server_id):
     conn.commit()
     conn.close()
     print(cur.rowcount, "Canal creado")  
-
+# funcionando
 def create_message(content,user_id,channel_id):
     query = "INSERT INTO messages (content,user_id,channel_id) VALUES (%s,%s,%s)"
     conn = bd.conectar()
@@ -44,5 +46,14 @@ def create_message(content,user_id,channel_id):
     conn.close()
     print(cur.rowcount, "Mensaje creado")
 
+def get_user(username):
+    query = "SELECT * FROM users WHERE username = %s"
+    conn = bd.conectar()
+    cur = conn.cursor()
+    val = (username,)
+    cur.execute(query,val)
+    conn.commit()
+    conn.close()
+    return cur.fetchone()
 
-create_message("hola","1","1")
+create_message("basura japishh!!",1,2)
