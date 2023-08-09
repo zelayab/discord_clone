2860a19886900168ffd01c5b8f679fd51f2fbc28
nazzgrelitod10s <ecosdesuperacion@gmail.com>
2023-08-08 23:02:26 -0300

    html_deforbidden_403

---
+++ datos.py
@@ -0,0 +1,10 @@
+
+#Credenciales de la base de datos
+
+credenciales = {
+    'user': 'devsis',
+    'password': 'Saltc3rr0SIT/',
+    'host': 'facu.saltait.com',
+    'database': 'discord_clone',
+    'port': '63006',
+}
